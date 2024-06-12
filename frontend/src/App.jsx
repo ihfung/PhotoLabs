83159ff50fa78edb465f80aebfe0f32f4afc922d
ihleung <ihfungleung@gmail.com>
2024-06-12 16:44:22 -0400

    Display 3 Photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,23 +20,22 @@ const sampleDataForPhotoListItem = {
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const photos = new Array(3);
+ const photos = new Array(3);
+ const photoItems = photos.map((photo, index) => (
+  <PhotoListItem
+    key={index}
+    location={sampleDataForPhotoListItem.location}
+    imageSource={sampleDataForPhotoListItem.imageSource}
+    username={sampleDataForPhotoListItem.username}
+    profile={sampleDataForPhotoListItem.profile}
+  />
+));
 
-  return (
-   
-    <div className="App">
-      
-    
-        <PhotoListItem
-        key={index}
-        location={sampleDataForPhotoListItem.location}
-        imageSource={sampleDataForPhotoListItem.imageSource}
-        username={sampleDataForPhotoListItem.username}
-        profile={sampleDataForPhotoListItem.profile}
-      />
-     
-    </div>
-  );
+return (
+  <div className="App">
+    {photoItems}
+  </div>
+);
 };
 
 export default App;
