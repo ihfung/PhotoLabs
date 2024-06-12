b31c45ff35373cd8c1c894d8a5f5dd544b650615
ihleung <ihfungleung@gmail.com>
2024-06-12 18:19:52 -0400

    updated fav button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,13 +4,21 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
-  const [isFav, setIsFav] = useState(false);
-  const fav =  () => setIsFav((isFav === true) ? false : true);
+  const [isFav, setIsFav] = useState("inactive");
+ const handClick = () => { setIsFav(isFav === "inactive" ? "active" : "inactive"); 
+    /*
+    if (isFav === "inactive") {
+      setIsFav("active");
+    } else {
+      setIsFav("inactive");
+    }
+    */
+  };
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-        <button onClick={fav}></button>
+      <div className={`photo-list__fav-icon-svg ${isFav === 'active' ? 'active' : 'inactive'}`} onClick={handClick}>
+        <FavIcon />
       </div>
     </div>
   );
