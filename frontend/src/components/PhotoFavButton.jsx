9284ffd59c93c0220b7a6d2c2cf64c749346851b
ihleung <ihfungleung@gmail.com>
2024-06-12 19:31:19 -0400

    updated fav button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -19,7 +19,7 @@ function PhotoFavButton() {
 
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg" onClick={handClick}>
+      <div className="photo-list__fav-icon-svg" onClick={handleClick}>
         <FavIcon />
       </div>
     </div>
