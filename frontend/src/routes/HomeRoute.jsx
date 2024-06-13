032847709e7b1c61904b9f1c7ac7657c69754eee
ihleung <ihfungleung@gmail.com>
2024-06-13 15:54:56 -0400

    updated the home route

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -6,6 +6,7 @@ import  '../styles/HomeRoute.scss';
 const HomeRoute = ({photos, topics}) => {
   //Update the app so that we can keep track of which photos the user has selected as favourites.
   //Consider storing this information in the HomeRoute.
+  const [favourites, setFavorites] = useState([]);
   const toggleFav = (photoId) => {
     setFavorites((prevFavorites) =>
       prevFavorites.includes(photoId)
