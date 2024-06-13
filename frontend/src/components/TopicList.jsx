be5fefa49a707374da7f42988e517217256148fe
ihleung <ihfungleung@gmail.com>
2024-06-13 13:59:05 -0400

    consume the mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,24 +2,9 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "../mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const sampleDataForTopicList = topics;
 
 const TopicList = () => {
   return (
