e14e00e93b35ea7b4125624210f1c5d3fade06f3
ihleung <ihfungleung@gmail.com>
2024-06-13 09:30:17 -0400

    comment in fav button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,20 +4,14 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
-  const [isFav, setIsFav] = useState(false);
+  //array destructuring is used to create a state variable and a function to update the state variable
+  const [isFav, setIsFav] = useState(false); //array destructuring, isFav is the state variable and setIsFav is the function to update the state variable
   const handleClick = () => {
     
     setIsFav((prevIsFav) => !prevIsFav); // toggle the state of isFav from true to false or vice versa
     console.log(isFav);
   };
-    /*
-    if (isFav === "inactive") {
-      setIsFav("active");
-    } else {
-      setIsFav("inactive");
-    }
-    */
-  
+   
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg" onClick={handleClick}>
