8b6bf27bc3d8f79b439266ec36a5c36c78fd6858
ihleung <ihfungleung@gmail.com>
2024-06-14 18:13:38 -0400

    add the hook for useapplicationdata

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import useApplicationData from './hooks/useApplicationData';
 //import PhotoListItem from './components/PhotoListItem';
 //import PhotoList from './components/PhotoList';
 //import TopNavigationBar from './components/TopNavigationBar';   
@@ -15,24 +15,29 @@ const App = () => {
   const [isFav, setIsFav] = useState([]);
   const [closeModal, setCloseModal] = useState(false); //displayModal = false by default setDisplayModal = function to change the value of displayModal
   const [selectedPhoto, setSelectedPhoto] = useState(null); //selectedPhoto = null by default setSelectedPhoto = function to change the value of selectedPhoto
+  const {
+    toggleFav,
+    toggleModal,
+    handleSelectPhoto
+  } = useApplicationData();
 
-  const toggleFav = (photoId) => {
-    if (isFav.includes(photoId)) {
-      setIsFav(isFav.filter((id) => id !== photoId));
-    } else {
-      setIsFav([...isFav, photoId]);
-    }
-  };
+  // const toggleFav = (photoId) => {
+  //   if (isFav.includes(photoId)) {
+  //     setIsFav(isFav.filter((id) => id !== photoId));
+  //   } else {
+  //     setIsFav([...isFav, photoId]);
+  //   }
+  // };
 
-  const toggleModal = () => {
-    setSelectedPhoto(null); //set selectedPhoto to null
-    setCloseModal(!closeModal); //toggle the value of displayModal to true or false
-  };
+  // const toggleModal = () => {
+  //   setSelectedPhoto(null); //set selectedPhoto to null
+  //   setCloseModal(!closeModal); //toggle the value of displayModal to true or false
+  // };
 
-  const handleSelectPhoto = (photoDetails) => {
-    setSelectedPhoto(photoDetails);
-    setCloseModal(true);
-  }
+  // const handleSelectPhoto = (photoDetails) => {
+  //   setSelectedPhoto(photoDetails);
+  //   setCloseModal(true);
+  // }
 
   // const selectedPhotoGroup = mockPhotos.find((photo) => photo.id === selectedPhoto);
 
---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+export default function useApplicationData() {
+  const [isFav, setIsFav] = useState([]);
+  const [closeModal, setCloseModal] = useState(false); //displayModal = false by default setDisplayModal = function to change the value of displayModal
+  const [selectedPhoto, setSelectedPhoto] = useState(null); //selectedPhoto = null by default setSelectedPhoto = function to change the value of selectedPhoto
+
+  const toggleFav = (photoId) => {
+    if (isFav.includes(photoId)) {
+      setIsFav(isFav.filter((id) => id !== photoId));
+    } else {
+      setIsFav([...isFav, photoId]);
+    }
+  };
+
+  const toggleModal = () => {
+    setSelectedPhoto(null); //set selectedPhoto to null
+    setCloseModal(!closeModal); //toggle the value of displayModal to true or false
+  };
+
+  const handleSelectPhoto = (photoDetails) => {
+    setSelectedPhoto(photoDetails);
+    setCloseModal(true);
+  }
+
+  return {
+    toggleFav,
+    toggleModal,
+    handleSelectPhoto,
+    selectedPhoto
+  };
+}
