1b768878b754512b0148a750577dc23f97b559bf
ihleung <ihfungleung@gmail.com>
2024-06-14 11:07:30 -0400

    added the modal photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -11,8 +11,17 @@ const PhotoDetailsModal = (props) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <div className="photo-details-modal__top-bar">
+        <img className="photo-details-modal__image" src={props.selectPhoto.imageSource} alt={`Photo by ${props.selectPhoto.username}`} />
+        <div className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={props.selectPhoto.profile} alt={`Profile picture of ${props.selectPhoto.username}`} />
+          <div className="photo-details-modal__photographer-info">
+            <p className="photo-details-modal__header">{props.selectPhoto.username}</p>
+            <p className="photo-details-modal__photographer-location">
+              {props.selectPhoto.location.city}, {props.selectPhoto.location.country}
+            </p>
+          </div>
+        </div>
       </div>
-        
     </div>
   )
 };
