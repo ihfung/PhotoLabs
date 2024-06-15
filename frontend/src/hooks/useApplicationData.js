d428f520a3c6320836d65773b1913bf27f0c40fb
ihleung <ihfungleung@gmail.com>
2024-06-14 20:49:26 -0400

    updated reducer as per compass feedback

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, { useReducer } from 'react';
 
 export const ACTIONS = {
   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
@@ -6,56 +6,56 @@ export const ACTIONS = {
   SET_PHOTO_DATA: 'SET_PHOTO_DATA',
   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
   SELECT_PHOTO: 'SELECT_PHOTO',
-  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
-}
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+  TOGGLE_MODAL: 'TOGGLE_MODAL'
+};
 
-
-export default function useApplicationData () {
-
-  
+const initialState = {
+  isFav: [],
+  closeModal: false,
+  selectedPhoto: null
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case FAV_PHOTO_ADDED:
-      return { /* insert logic */ }
-    { /* insert all relevant actions as case statements*/ }  
-    }
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return { ...state, isFav: [...state.isFav, action.payload] };
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return { ...state, isFav: state.isFav.filter((id) => id !== action.payload) };
+    case ACTIONS.SELECT_PHOTO:
+      return { ...state, selectedPhoto: action.payload, closeModal: true };
+    case ACTIONS.TOGGLE_MODAL:
+      return { ...state, selectedPhoto: null, closeModal: !state.closeModal };
     default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
+      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
   }
 }
-  
-  const [isFav, setIsFav] = useState([]);
-  const [closeModal, setCloseModal] = useState(false); //displayModal = false by default setDisplayModal = function to change the value of displayModal
-  const [selectedPhoto, setSelectedPhoto] = useState(null); //selectedPhoto = null by default setSelectedPhoto = function to change the value of selectedPhoto
+
+export default function useApplicationData() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFav = (photoId) => {
-    if (isFav.includes(photoId)) {
-      setIsFav(isFav.filter((id) => id !== photoId));
+    if (state.isFav.includes(photoId)) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: photoId });
     } else {
-      setIsFav([...isFav, photoId]);
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photoId });
     }
   };
 
   const toggleModal = () => {
-    setSelectedPhoto(null); //set selectedPhoto to null
-    setCloseModal(!closeModal); //toggle the value of displayModal to true or false
+    dispatch({ type: ACTIONS.TOGGLE_MODAL });
   };
 
   const handleSelectPhoto = (photoDetails) => {
-    setSelectedPhoto(photoDetails);
-    setCloseModal(true);
-  }
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: photoDetails });
+  };
 
   return {
     toggleFav,
     toggleModal,
     handleSelectPhoto,
-    selectedPhoto,
-    isFav,
-    closeModal
+    selectedPhoto: state.selectedPhoto,
+    isFav: state.isFav,
+    closeModal: state.closeModal
   };
 }
-
