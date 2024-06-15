5db5df111f5b3901c31d1c716065166e4e943958
ihleung <ihfungleung@gmail.com>
2024-06-14 20:46:47 -0400

    added reducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,6 +1,32 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
 
 export default function useApplicationData () {
+
+  
+
+function reducer(state, action) {
+  switch (action.type) {
+    case FAV_PHOTO_ADDED:
+      return { /* insert logic */ }
+    { /* insert all relevant actions as case statements*/ }  
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+  
   const [isFav, setIsFav] = useState([]);
   const [closeModal, setCloseModal] = useState(false); //displayModal = false by default setDisplayModal = function to change the value of displayModal
   const [selectedPhoto, setSelectedPhoto] = useState(null); //selectedPhoto = null by default setSelectedPhoto = function to change the value of selectedPhoto
