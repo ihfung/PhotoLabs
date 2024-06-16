9a32eb84ae61b1cafa06746ef82fc7b11028d702
ihleung <ihfungleung@gmail.com>
2024-06-16 14:26:37 -0400

    fix styling on the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -7,24 +7,28 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = (props) => {
   //console.log(props.selectPhoto);
   return (
+    
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick= {() => {props.toggleModal()}}>
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-    <div>
-        <PhotoFavButton 
-          photoId={props.selectPhoto.id}
-          isFav={props.isFav.includes(props.selectPhoto.id)} // includes is a method that checks if an array includes a certain value
-          toggleFav={props.toggleFav}
-          handleSelectPhoto={props.handleSelectPhoto}
+       <button className="photo-details-modal__close-button" onClick= {() => {props.toggleModal()}}>
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+    <div className="detail-image">
+       
+          
+          <PhotoFavButton 
+            photoId={props.selectPhoto.id}
+            isFav={props.isFav.includes(props.selectPhoto.id)} // includes is a method that checks if an array includes a certain value
+            toggleFav={props.toggleFav}
+            handleSelectPhoto={props.handleSelectPhoto}
           />
+       
         <img
-          className="photo-details-modal--images"
+          className="photo-details-modal__image"
           src={props.selectPhoto.urls.regular}
           alt="Photo"
         />
         
-        <div className="photo-details-modal--photographer-details">
+        <div className="photo-details-modal__photographer-details">
           <img className="photo-details-modal__photographer-profile" src={props.selectPhoto.user.profile} />
           <div className="photo-details-modal__photographer-info">
               <p>{props.selectPhoto.user.username}</p>
@@ -40,7 +44,7 @@ const PhotoDetailsModal = (props) => {
         
       </div>
       <hr></hr>
-      <header className="photo-details-modal--header">Similar photos</header>
+      <header className="photo-details-modal__header">Similar photos</header>
       <PhotoList photos={props.selectedPhotoGroup} isFav={props.isFav}
       toggleFav={props.toggleFav}/>
     </div>
